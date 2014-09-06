0d3f603a3a6d7822ec6d9489ae8d690de27c37ca
Winnie Tong <tong.winnie@gmail.com>
2014-09-05 21:19:00 -0700

    Edit settings.py

--- stockproject/settings.py
+++ stockproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '^ihtgspjf5k^%+=#6t#$rp)=34al7+6%sqcs*4mo$v^juiz$8l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
